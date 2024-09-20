e5f93a09d7723e8b0d61874d3438832d552131ef
Colum Ferry <cferry09@gmail.com>
2024-09-20 10:49:25 +0100

    feat: add libraries

--- apps/myapp/src/app/app.routes.ts
+++ apps/myapp/src/app/app.routes.ts
@@ -1,3 +1,504 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'lib100',
+    loadChildren: () =>
+      import('@angular-rspack/lib100').then((m) => m.lib100Routes),
+  },
+  {
+    path: 'lib99',
+    loadChildren: () =>
+      import('@angular-rspack/lib99').then((m) => m.lib99Routes),
+  },
+  {
+    path: 'lib98',
+    loadChildren: () =>
+      import('@angular-rspack/lib98').then((m) => m.lib98Routes),
+  },
+  {
+    path: 'lib97',
+    loadChildren: () =>
+      import('@angular-rspack/lib97').then((m) => m.lib97Routes),
+  },
+  {
+    path: 'lib96',
+    loadChildren: () =>
+      import('@angular-rspack/lib96').then((m) => m.lib96Routes),
+  },
+  {
+    path: 'lib95',
+    loadChildren: () =>
+      import('@angular-rspack/lib95').then((m) => m.lib95Routes),
+  },
+  {
+    path: 'lib94',
+    loadChildren: () =>
+      import('@angular-rspack/lib94').then((m) => m.lib94Routes),
+  },
+  {
+    path: 'lib93',
+    loadChildren: () =>
+      import('@angular-rspack/lib93').then((m) => m.lib93Routes),
+  },
+  {
+    path: 'lib92',
+    loadChildren: () =>
+      import('@angular-rspack/lib92').then((m) => m.lib92Routes),
+  },
+  {
+    path: 'lib91',
+    loadChildren: () =>
+      import('@angular-rspack/lib91').then((m) => m.lib91Routes),
+  },
+  {
+    path: 'lib90',
+    loadChildren: () =>
+      import('@angular-rspack/lib90').then((m) => m.lib90Routes),
+  },
+  {
+    path: 'lib89',
+    loadChildren: () =>
+      import('@angular-rspack/lib89').then((m) => m.lib89Routes),
+  },
+  {
+    path: 'lib88',
+    loadChildren: () =>
+      import('@angular-rspack/lib88').then((m) => m.lib88Routes),
+  },
+  {
+    path: 'lib87',
+    loadChildren: () =>
+      import('@angular-rspack/lib87').then((m) => m.lib87Routes),
+  },
+  {
+    path: 'lib86',
+    loadChildren: () =>
+      import('@angular-rspack/lib86').then((m) => m.lib86Routes),
+  },
+  {
+    path: 'lib85',
+    loadChildren: () =>
+      import('@angular-rspack/lib85').then((m) => m.lib85Routes),
+  },
+  {
+    path: 'lib84',
+    loadChildren: () =>
+      import('@angular-rspack/lib84').then((m) => m.lib84Routes),
+  },
+  {
+    path: 'lib83',
+    loadChildren: () =>
+      import('@angular-rspack/lib83').then((m) => m.lib83Routes),
+  },
+  {
+    path: 'lib82',
+    loadChildren: () =>
+      import('@angular-rspack/lib82').then((m) => m.lib82Routes),
+  },
+  {
+    path: 'lib81',
+    loadChildren: () =>
+      import('@angular-rspack/lib81').then((m) => m.lib81Routes),
+  },
+  {
+    path: 'lib80',
+    loadChildren: () =>
+      import('@angular-rspack/lib80').then((m) => m.lib80Routes),
+  },
+  {
+    path: 'lib79',
+    loadChildren: () =>
+      import('@angular-rspack/lib79').then((m) => m.lib79Routes),
+  },
+  {
+    path: 'lib78',
+    loadChildren: () =>
+      import('@angular-rspack/lib78').then((m) => m.lib78Routes),
+  },
+  {
+    path: 'lib77',
+    loadChildren: () =>
+      import('@angular-rspack/lib77').then((m) => m.lib77Routes),
+  },
+  {
+    path: 'lib76',
+    loadChildren: () =>
+      import('@angular-rspack/lib76').then((m) => m.lib76Routes),
+  },
+  {
+    path: 'lib75',
+    loadChildren: () =>
+      import('@angular-rspack/lib75').then((m) => m.lib75Routes),
+  },
+  {
+    path: 'lib74',
+    loadChildren: () =>
+      import('@angular-rspack/lib74').then((m) => m.lib74Routes),
+  },
+  {
+    path: 'lib73',
+    loadChildren: () =>
+      import('@angular-rspack/lib73').then((m) => m.lib73Routes),
+  },
+  {
+    path: 'lib72',
+    loadChildren: () =>
+      import('@angular-rspack/lib72').then((m) => m.lib72Routes),
+  },
+  {
+    path: 'lib71',
+    loadChildren: () =>
+      import('@angular-rspack/lib71').then((m) => m.lib71Routes),
+  },
+  {
+    path: 'lib70',
+    loadChildren: () =>
+      import('@angular-rspack/lib70').then((m) => m.lib70Routes),
+  },
+  {
+    path: 'lib69',
+    loadChildren: () =>
+      import('@angular-rspack/lib69').then((m) => m.lib69Routes),
+  },
+  {
+    path: 'lib68',
+    loadChildren: () =>
+      import('@angular-rspack/lib68').then((m) => m.lib68Routes),
+  },
+  {
+    path: 'lib67',
+    loadChildren: () =>
+      import('@angular-rspack/lib67').then((m) => m.lib67Routes),
+  },
+  {
+    path: 'lib66',
+    loadChildren: () =>
+      import('@angular-rspack/lib66').then((m) => m.lib66Routes),
+  },
+  {
+    path: 'lib65',
+    loadChildren: () =>
+      import('@angular-rspack/lib65').then((m) => m.lib65Routes),
+  },
+  {
+    path: 'lib64',
+    loadChildren: () =>
+      import('@angular-rspack/lib64').then((m) => m.lib64Routes),
+  },
+  {
+    path: 'lib63',
+    loadChildren: () =>
+      import('@angular-rspack/lib63').then((m) => m.lib63Routes),
+  },
+  {
+    path: 'lib62',
+    loadChildren: () =>
+      import('@angular-rspack/lib62').then((m) => m.lib62Routes),
+  },
+  {
+    path: 'lib61',
+    loadChildren: () =>
+      import('@angular-rspack/lib61').then((m) => m.lib61Routes),
+  },
+  {
+    path: 'lib60',
+    loadChildren: () =>
+      import('@angular-rspack/lib60').then((m) => m.lib60Routes),
+  },
+  {
+    path: 'lib59',
+    loadChildren: () =>
+      import('@angular-rspack/lib59').then((m) => m.lib59Routes),
+  },
+  {
+    path: 'lib58',
+    loadChildren: () =>
+      import('@angular-rspack/lib58').then((m) => m.lib58Routes),
+  },
+  {
+    path: 'lib57',
+    loadChildren: () =>
+      import('@angular-rspack/lib57').then((m) => m.lib57Routes),
+  },
+  {
+    path: 'lib56',
+    loadChildren: () =>
+      import('@angular-rspack/lib56').then((m) => m.lib56Routes),
+  },
+  {
+    path: 'lib55',
+    loadChildren: () =>
+      import('@angular-rspack/lib55').then((m) => m.lib55Routes),
+  },
+  {
+    path: 'lib54',
+    loadChildren: () =>
+      import('@angular-rspack/lib54').then((m) => m.lib54Routes),
+  },
+  {
+    path: 'lib53',
+    loadChildren: () =>
+      import('@angular-rspack/lib53').then((m) => m.lib53Routes),
+  },
+  {
+    path: 'lib52',
+    loadChildren: () =>
+      import('@angular-rspack/lib52').then((m) => m.lib52Routes),
+  },
+  {
+    path: 'lib51',
+    loadChildren: () =>
+      import('@angular-rspack/lib51').then((m) => m.lib51Routes),
+  },
+  {
+    path: 'lib50',
+    loadChildren: () =>
+      import('@angular-rspack/lib50').then((m) => m.lib50Routes),
+  },
+  {
+    path: 'lib49',
+    loadChildren: () =>
+      import('@angular-rspack/lib49').then((m) => m.lib49Routes),
+  },
+  {
+    path: 'lib48',
+    loadChildren: () =>
+      import('@angular-rspack/lib48').then((m) => m.lib48Routes),
+  },
+  {
+    path: 'lib47',
+    loadChildren: () =>
+      import('@angular-rspack/lib47').then((m) => m.lib47Routes),
+  },
+  {
+    path: 'lib46',
+    loadChildren: () =>
+      import('@angular-rspack/lib46').then((m) => m.lib46Routes),
+  },
+  {
+    path: 'lib45',
+    loadChildren: () =>
+      import('@angular-rspack/lib45').then((m) => m.lib45Routes),
+  },
+  {
+    path: 'lib44',
+    loadChildren: () =>
+      import('@angular-rspack/lib44').then((m) => m.lib44Routes),
+  },
+  {
+    path: 'lib43',
+    loadChildren: () =>
+      import('@angular-rspack/lib43').then((m) => m.lib43Routes),
+  },
+  {
+    path: 'lib42',
+    loadChildren: () =>
+      import('@angular-rspack/lib42').then((m) => m.lib42Routes),
+  },
+  {
+    path: 'lib41',
+    loadChildren: () =>
+      import('@angular-rspack/lib41').then((m) => m.lib41Routes),
+  },
+  {
+    path: 'lib40',
+    loadChildren: () =>
+      import('@angular-rspack/lib40').then((m) => m.lib40Routes),
+  },
+  {
+    path: 'lib39',
+    loadChildren: () =>
+      import('@angular-rspack/lib39').then((m) => m.lib39Routes),
+  },
+  {
+    path: 'lib38',
+    loadChildren: () =>
+      import('@angular-rspack/lib38').then((m) => m.lib38Routes),
+  },
+  {
+    path: 'lib37',
+    loadChildren: () =>
+      import('@angular-rspack/lib37').then((m) => m.lib37Routes),
+  },
+  {
+    path: 'lib36',
+    loadChildren: () =>
+      import('@angular-rspack/lib36').then((m) => m.lib36Routes),
+  },
+  {
+    path: 'lib35',
+    loadChildren: () =>
+      import('@angular-rspack/lib35').then((m) => m.lib35Routes),
+  },
+  {
+    path: 'lib34',
+    loadChildren: () =>
+      import('@angular-rspack/lib34').then((m) => m.lib34Routes),
+  },
+  {
+    path: 'lib33',
+    loadChildren: () =>
+      import('@angular-rspack/lib33').then((m) => m.lib33Routes),
+  },
+  {
+    path: 'lib32',
+    loadChildren: () =>
+      import('@angular-rspack/lib32').then((m) => m.lib32Routes),
+  },
+  {
+    path: 'lib31',
+    loadChildren: () =>
+      import('@angular-rspack/lib31').then((m) => m.lib31Routes),
+  },
+  {
+    path: 'lib30',
+    loadChildren: () =>
+      import('@angular-rspack/lib30').then((m) => m.lib30Routes),
+  },
+  {
+    path: 'lib29',
+    loadChildren: () =>
+      import('@angular-rspack/lib29').then((m) => m.lib29Routes),
+  },
+  {
+    path: 'lib28',
+    loadChildren: () =>
+      import('@angular-rspack/lib28').then((m) => m.lib28Routes),
+  },
+  {
+    path: 'lib27',
+    loadChildren: () =>
+      import('@angular-rspack/lib27').then((m) => m.lib27Routes),
+  },
+  {
+    path: 'lib26',
+    loadChildren: () =>
+      import('@angular-rspack/lib26').then((m) => m.lib26Routes),
+  },
+  {
+    path: 'lib25',
+    loadChildren: () =>
+      import('@angular-rspack/lib25').then((m) => m.lib25Routes),
+  },
+  {
+    path: 'lib24',
+    loadChildren: () =>
+      import('@angular-rspack/lib24').then((m) => m.lib24Routes),
+  },
+  {
+    path: 'lib23',
+    loadChildren: () =>
+      import('@angular-rspack/lib23').then((m) => m.lib23Routes),
+  },
+  {
+    path: 'lib22',
+    loadChildren: () =>
+      import('@angular-rspack/lib22').then((m) => m.lib22Routes),
+  },
+  {
+    path: 'lib21',
+    loadChildren: () =>
+      import('@angular-rspack/lib21').then((m) => m.lib21Routes),
+  },
+  {
+    path: 'lib20',
+    loadChildren: () =>
+      import('@angular-rspack/lib20').then((m) => m.lib20Routes),
+  },
+  {
+    path: 'lib19',
+    loadChildren: () =>
+      import('@angular-rspack/lib19').then((m) => m.lib19Routes),
+  },
+  {
+    path: 'lib18',
+    loadChildren: () =>
+      import('@angular-rspack/lib18').then((m) => m.lib18Routes),
+  },
+  {
+    path: 'lib17',
+    loadChildren: () =>
+      import('@angular-rspack/lib17').then((m) => m.lib17Routes),
+  },
+  {
+    path: 'lib16',
+    loadChildren: () =>
+      import('@angular-rspack/lib16').then((m) => m.lib16Routes),
+  },
+  {
+    path: 'lib15',
+    loadChildren: () =>
+      import('@angular-rspack/lib15').then((m) => m.lib15Routes),
+  },
+  {
+    path: 'lib14',
+    loadChildren: () =>
+      import('@angular-rspack/lib14').then((m) => m.lib14Routes),
+  },
+  {
+    path: 'lib13',
+    loadChildren: () =>
+      import('@angular-rspack/lib13').then((m) => m.lib13Routes),
+  },
+  {
+    path: 'lib12',
+    loadChildren: () =>
+      import('@angular-rspack/lib12').then((m) => m.lib12Routes),
+  },
+  {
+    path: 'lib11',
+    loadChildren: () =>
+      import('@angular-rspack/lib11').then((m) => m.lib11Routes),
+  },
+  {
+    path: 'lib10',
+    loadChildren: () =>
+      import('@angular-rspack/lib10').then((m) => m.lib10Routes),
+  },
+  {
+    path: 'lib9',
+    loadChildren: () =>
+      import('@angular-rspack/lib9').then((m) => m.lib9Routes),
+  },
+  {
+    path: 'lib8',
+    loadChildren: () =>
+      import('@angular-rspack/lib8').then((m) => m.lib8Routes),
+  },
+  {
+    path: 'lib7',
+    loadChildren: () =>
+      import('@angular-rspack/lib7').then((m) => m.lib7Routes),
+  },
+  {
+    path: 'lib6',
+    loadChildren: () =>
+      import('@angular-rspack/lib6').then((m) => m.lib6Routes),
+  },
+  {
+    path: 'lib5',
+    loadChildren: () =>
+      import('@angular-rspack/lib5').then((m) => m.lib5Routes),
+  },
+  {
+    path: 'lib4',
+    loadChildren: () =>
+      import('@angular-rspack/lib4').then((m) => m.lib4Routes),
+  },
+  {
+    path: 'lib3',
+    loadChildren: () =>
+      import('@angular-rspack/lib3').then((m) => m.lib3Routes),
+  },
+  {
+    path: 'lib2',
+    loadChildren: () =>
+      import('@angular-rspack/lib2').then((m) => m.lib2Routes),
+  },
+  {
+    path: 'lib1',
+    loadChildren: () =>
+      import('@angular-rspack/lib1').then((m) => m.lib1Routes),
+  },
+];
