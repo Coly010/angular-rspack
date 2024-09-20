bad037010d34c8a683d5c85b2f4ca6f9456d6d99
Colum Ferry <cferry09@gmail.com>
2024-09-20 10:24:55 +0100

    feat: add generate libs script

--- apps/myapp/src/app/app.component.ts
+++ apps/myapp/src/app/app.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [NxWelcomeComponent, RouterModule, FormsModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
+  private router = inject(Router);
   title = 'myapp';
+  routeToNavigateTo = "";
+
+  navigate() {
+    this.router.navigate([`/${this.routeToNavigateTo}`]);
+  }
 }
---
+++ tools/generate-libs.ts
@@ -0,0 +1,49 @@
+import { execSync } from 'child_process';
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+import { logger } from 'nx/src/utils/logger';
+import { workspaceRoot } from '@nx/devkit';
+
+const STARTING_LIB_INDEX = 0;
+const NUMBER_OF_LIBS = 2;
+const APP_TO_ATTACH_LIBS = 'myapp';
+const PATH_TO_PARENT_ROUTES = `apps/${APP_TO_ATTACH_LIBS}/src/app/app.routes.ts`;
+const PATH_TO_APP_COMPONENT_TEMPLATE = `apps/${APP_TO_ATTACH_LIBS}/src/app/app.component.html`;
+const OPTIONS_SELECTOR = `<!-- OPTIONS -->`;
+
+function main() {
+  const options: { name: string; route: string }[] = [];
+
+  logger.info(`NX Generating Libraries...`);
+  for (let i = STARTING_LIB_INDEX; i < NUMBER_OF_LIBS; i++) {
+    const libName = `lib${i + 1}`;
+    const command = `nx g @nx/angular:library ${libName} --directory=libs/${libName} --routing=true --parent=${PATH_TO_PARENT_ROUTES} --lazy --standalone --flat --projectNameAndRootFormat=as-provided --no-interactive`;
+    execSync(`npx ${command}`, { stdio: 'ignore' });
+    options.push({
+      name: libName,
+      route: libName,
+    });
+  }
+  logger.info(`NX Successfully generated libraries`);
+  logger.info(`NX Creating Route Selector...`);
+  const appComponentContents = readFileSync(
+    join(workspaceRoot, PATH_TO_APP_COMPONENT_TEMPLATE),
+    'utf8'
+  );
+  const optionsStartingIndex =
+    appComponentContents.indexOf(OPTIONS_SELECTOR) + OPTIONS_SELECTOR.length;
+  const newAppComponentContents = `${appComponentContents.slice(
+    0,
+    optionsStartingIndex
+  )}${options.reduce(
+    (acc, o) => `${acc}<option value="${o.route}">${o.name}</option>\n`,
+    ''
+  )}${appComponentContents.slice(optionsStartingIndex)}`;
+  writeFileSync(
+    join(workspaceRoot, PATH_TO_APP_COMPONENT_TEMPLATE),
+    newAppComponentContents
+  );
+  logger.info(`NX Successfully created route selector`);
+}
+
+main();
