8f012fe6f4bb1f1346ee479df45e6390370177be
Colum Ferry <cferry09@gmail.com>
2024-09-22 20:37:30 +0100

    use @ng-rspack/build createConfig

--- apps/myapp/rspack.config.ts
+++ apps/myapp/rspack.config.ts
@@ -1,123 +1,13 @@
-import { Compiler, Configuration, DefinePlugin, HtmlRspackPlugin, SwcJsMinimizerRspackPlugin } from '@rspack/core';
-import { join, resolve } from 'path';
-import { workspaceRoot } from '@nx/devkit';
-import { AngularRspackPlugin } from '@ng-rspack/build';
+import { createConfig } from '@ng-rspack/build';
 
-export class RxJsEsmResolutionPlugin {
-  apply(compiler: Compiler) {
-    compiler.hooks.normalModuleFactory.tap('PlUGIN', (normalModuleFactory) => {
-      normalModuleFactory.hooks.resolve.tap('PLUGIN', (data) => {
-        if (data.request.startsWith("rxjs")) {
-          data.request = data.request.replace(/([\\/]dist[\\/])cjs([\\/])/, '$1esm$2');
-        }
-      })
-    })
-  }
-}
-
-const config: Configuration = {
-  context: __dirname,
-  mode: 'production',
-  target: 'web',
-  entry: {
-    main: './src/main.ts',
-    polyfills: ['zone.js'],
-  },
-  output: {
-    uniqueName: 'myapp',
-    hashFunction: 'xxhash64',
-    clean: true,
-    path: resolve(workspaceRoot, 'dist/apps/myapp'),
-    filename: '[name].[contenthash].js',
-    chunkFilename: '[name].[contenthash].js',
-    crossOriginLoading: false,
-    trustedTypes: 'angular#bundler',
-    scriptType: 'module',
-    module: true
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.mjs', '.js'],
-    modules: ['node_modules'],
-    mainFields: ['es2020', 'es2015', 'browser', 'module', 'main'],
-    conditionNames: ['es2020', 'es2015', '...'],
-    tsConfig: resolve(__dirname, './tsconfig.app.json'),
-  },
-  optimization: {
-    minimize: true,
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      minChunks: 1,
-      minSize: 20000,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-          reuseExistingChunk: true,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
-    minimizer: [new SwcJsMinimizerRspackPlugin()]
-  },
-  module: {
-    parser: {
-      javascript: {
-        requireContext: false,
-        url: false,
-      },
-    },
-    rules: [
-      {
-        test: /\.[cm]?[jt]sx?$/,
-        use: [
-          {
-            loader: 'builtin:swc-loader',
-            options: {
-              jsc: {
-                parser: {
-                  syntax: 'typescript',
-                },
-              },
-            },
-          },
-          {
-            loader: require.resolve("@ng-rspack/build/src/lib/loaders/angular-loader.js"),
-          },
-        ],
-      },
-      {
-        test: /\.[cm]?js$/,
-        use: [
-          {
-            loader: require.resolve("@ng-rspack/build/src/lib/loaders/js-loader.js"),
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new DefinePlugin({
-      ngDevMode: 'false',
-      ngJitMode: 'false',
-    }),
-    new HtmlRspackPlugin({
-      minify: false,
-      inject: 'body',
-      scriptLoading: 'module',
-      template: 'src/index.html',
-    }),
-    new RxJsEsmResolutionPlugin(),
-    new AngularRspackPlugin({
-      tsconfig: resolve(__dirname, './tsconfig.app.json'),
-    }),
-  ],
-};
-
-export default () => config;
+export default () => createConfig({
+  root: __dirname,
+  name: 'myapp',
+  main: './src/main.ts',
+  index: './src/index.html',
+  tsConfig: './tsconfig.app.json',
+  outputPath: '../../dist/apps/myapp',
+  styles: ['./src/styles.scss'],
+  assets: ['./public'],
+  polyfills: ['zone.js']
+});
--- apps/scssapp/rspack.config.ts
+++ apps/scssapp/rspack.config.ts
@@ -9,159 +9,165 @@ import {
 } from '@rspack/core';
 import { join, resolve } from 'path';
 import { workspaceRoot } from '@nx/devkit';
-import { AngularRspackPlugin } from '@ng-rspack/build';
+import { createConfig } from '@ng-rspack/build';
 
-export class RxJsEsmResolutionPlugin {
-  apply(compiler: Compiler) {
-    compiler.hooks.normalModuleFactory.tap('PlUGIN', (normalModuleFactory) => {
-      normalModuleFactory.hooks.resolve.tap('PLUGIN', (data) => {
-          if (data.request.startsWith("rxjs")) {
-            data.request = data.request.replace(/([\\/]dist[\\/])cjs([\\/])/, '$1esm$2');
-          }
-      })
-    })
-  }
-}
+// export class RxJsEsmResolutionPlugin {
+//   apply(compiler: Compiler) {
+//     compiler.hooks.thisCompilation.tap('PlUGIN', (compilation) => {
+//       console.log(">>>DBG>>>", compilation.options.entry)
+//     })
+//   }
+// }
 
-const config: Configuration = {
-  context: __dirname,
-  mode: 'production',
-  target: 'web',
-  cache: true,
-  entry: {
-    main: './src/main.ts',
-    polyfills: ['zone.js'],
-    styles: './src/styles.scss'
-  },
-  output: {
-    uniqueName: 'scssapp',
-    hashFunction: 'xxhash64',
-    publicPath: 'auto',
-    clean: true,
-    path: resolve(workspaceRoot, 'dist/apps/scssapp'),
-    cssFilename: '[name].[contenthash].css',
-    filename: '[name].[contenthash].js',
-    chunkFilename: '[name].[contenthash].js',
-    crossOriginLoading: false,
-    trustedTypes: 'angular#bundler',
-    scriptType: 'module',
-    module: true
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.mjs', '.js'],
-    modules: ['node_modules'],
-    mainFields: ['es2020', 'es2015', 'browser', 'module', 'main'],
-    conditionNames: ['es2020', 'es2015', '...'],
-    tsConfig: resolve(__dirname, './tsconfig.app.json'),
-  },
-  optimization: {
-    minimize: true,
-    runtimeChunk: 'single',
-    splitChunks: {
-      chunks: 'all',
-      minChunks: 1,
-      minSize: 20000,
-      maxAsyncRequests: 30,
-      maxInitialRequests: 30,
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-          reuseExistingChunk: true,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
-    minimizer: [new SwcJsMinimizerRspackPlugin()]
-  },
-  experiments: {
-    css: true
-  },
-  module: {
-    parser: {
-      javascript: {
-        requireContext: false,
-        url: false,
-      },
-      'css/auto': {
-        esModule: true
-      }
-    },
-    rules: [
-      {
-        test: /\.?(sa|sc|c)ss$/,
-        use: [
-          {
-            loader: 'sass-loader',
-            options: {
-              api: 'modern-compiler',
-              implementation: require.resolve('sass-embedded'),
-            },
-          },
-        ],
-        type: 'css/auto'
-      },
-      { test: /[/\\]rxjs[/\\]add[/\\].+\.js$/, sideEffects: true },
-      {
-        test: /\.[cm]?[jt]sx?$/,
-        use: [
-          {
-            loader: 'builtin:swc-loader',
-            options: {
-              jsc: {
-                parser: {
-                  syntax: 'typescript',
-                },
-              },
-            },
-          },
-          {
-            loader: require.resolve("@ng-rspack/build/src/lib/loaders/angular-loader.js"),
-          },
-        ],
-      },
-      {
-        test: /\.[cm]?js$/,
-        use: [
-          {
-            loader: require.resolve("@ng-rspack/build/src/lib/loaders/js-loader.js"),
-          },
-        ],
-      },
-    ],
-  },
-  plugins: [
-    new DefinePlugin({
-      ngDevMode: 'false',
-      ngJitMode: 'false',
-    }),
-    new CopyRspackPlugin({
-      patterns: [
-        {
-          from: 'public',
-          to: '.',
-          globOptions: {
-            dot: false,
-          },
-          noErrorOnMissing: true,
-        },
-      ],
-    }),
-    new HtmlRspackPlugin({
-      minify: false,
-      inject: 'body',
-      scriptLoading: 'module',
-      template: 'src/index.html',
-    }),
-    new RxJsEsmResolutionPlugin(),
-    new AngularRspackPlugin({
-      tsconfig: resolve(__dirname, './tsconfig.app.json'),
-    }),
-  ],
-};
+// const config: Configuration = {
+//   context: __dirname,
+//   mode: 'production',
+//   target: 'web',
+//   cache: true,
+//   entry: {
+//     main: './src/main.ts',
+//     polyfills: ['zone.js'],
+//     styles: './src/styles.scss'
+//   },
+//   output: {
+//     uniqueName: 'scssapp',
+//     hashFunction: 'xxhash64',
+//     publicPath: 'auto',
+//     clean: true,
+//     path: resolve(workspaceRoot, 'dist/apps/scssapp'),
+//     cssFilename: '[name].[contenthash].css',
+//     filename: '[name].[contenthash].js',
+//     chunkFilename: '[name].[contenthash].js',
+//     crossOriginLoading: false,
+//     trustedTypes: 'angular#bundler',
+//     scriptType: 'module',
+//     module: true
+//   },
+//   resolve: {
+//     extensions: ['.ts', '.tsx', '.mjs', '.js'],
+//     modules: ['node_modules'],
+//     mainFields: ['es2020', 'es2015', 'browser', 'module', 'main'],
+//     conditionNames: ['es2020', 'es2015', '...'],
+//     tsConfig: resolve(__dirname, './tsconfig.app.json'),
+//   },
+//   optimization: {
+//     minimize: true,
+//     runtimeChunk: 'single',
+//     splitChunks: {
+//       chunks: 'all',
+//       minChunks: 1,
+//       minSize: 20000,
+//       maxAsyncRequests: 30,
+//       maxInitialRequests: 30,
+//       cacheGroups: {
+//         defaultVendors: {
+//           test: /[\\/]node_modules[\\/]/,
+//           priority: -10,
+//           reuseExistingChunk: true,
+//         },
+//         default: {
+//           minChunks: 2,
+//           priority: -20,
+//           reuseExistingChunk: true,
+//         },
+//       },
+//     },
+//     minimizer: [new SwcJsMinimizerRspackPlugin()]
+//   },
+//   experiments: {
+//     css: true
+//   },
+//   module: {
+//     parser: {
+//       javascript: {
+//         requireContext: false,
+//         url: false,
+//       },
+//       'css/auto': {
+//         esModule: true
+//       }
+//     },
+//     rules: [
+//       {
+//         test: /\.?(sa|sc|c)ss$/,
+//         use: [
+//           {
+//             loader: 'sass-loader',
+//             options: {
+//               api: 'modern-compiler',
+//               implementation: require.resolve('sass-embedded'),
+//             },
+//           },
+//         ],
+//         type: 'css/auto'
+//       },
+//       { test: /[/\\]rxjs[/\\]add[/\\].+\.js$/, sideEffects: true },
+//       {
+//         test: /\.[cm]?[jt]sx?$/,
+//         use: [
+//           {
+//             loader: 'builtin:swc-loader',
+//             options: {
+//               jsc: {
+//                 parser: {
+//                   syntax: 'typescript',
+//                 },
+//               },
+//             },
+//           },
+//           {
+//             loader: require.resolve("@ng-rspack/build/src/lib/loaders/angular-loader.js"),
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.[cm]?js$/,
+//         use: [
+//           {
+//             loader: require.resolve("@ng-rspack/build/src/lib/loaders/js-loader.js"),
+//           },
+//         ],
+//       },
+//     ],
+//   },
+//   plugins: [
+//     new DefinePlugin({
+//       ngDevMode: 'false',
+//       ngJitMode: 'false',
+//     }),
+//     new CopyRspackPlugin({
+//       patterns: [
+//         {
+//           from: 'public',
+//           to: '.',
+//           globOptions: {
+//             dot: false,
+//           },
+//           noErrorOnMissing: true,
+//         },
+//       ],
+//     }),
+//     new HtmlRspackPlugin({
+//       minify: false,
+//       inject: 'body',
+//       scriptLoading: 'module',
+//       template: 'src/index.html',
+//     }),
+//     new RxJsEsmResolutionPlugin(),
+//     new AngularRspackPlugin({
+//       tsconfig: resolve(__dirname, './tsconfig.app.json'),
+//     }),
+//   ],
+// };
 
-export default () => config;
+export default () => createConfig({
+  root: __dirname,
+  name: 'scssapp',
+  main: './src/main.ts',
+  index: './src/index.html',
+  tsConfig: './tsconfig.app.json',
+  outputPath: '../../dist/apps/scssapp',
+  styles: ['./src/styles.scss'],
+  assets: ['./public'],
+  polyfills: ['zone.js']
+});
